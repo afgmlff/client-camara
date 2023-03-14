ed6527d6a9786f1b5d87eed76dba9d96c3374ff4
afgmlff <aeciofgm@gmail.com>
2023-03-13 21:06:12 -0300

    styling intro page

--- client/src/Intro.js
+++ client/src/Intro.js
@@ -9,12 +9,13 @@ function Intro() {
     <>
         <div className='container-fluid'>
             <div class="row">
-                <div className='col-sm-6 esquerda'>
+                <div className='col-lg-5 esquerda'>
                     <p className='title'>Dados Abertos</p>
                     <p className='parag'>Uma aplicação web que permite a pesquisa de informações sobre deputados federais da 56ª legislatura, a partir de um serviço de Dados Abertos da Câmara dos Deputados</p>
-                    <p className='linkAPI'><span class="material-symbols-outlined">open_in_new</span>Saiba mais sobre a API</p>
+                    <p className='linkAPI'><a href='https://dadosabertos.camara.leg.br/swagger/api.html' target="_blank"><span class="material-symbols-outlined">open_in_new</span><span className='linkText'>Saiba mais sobre a API</span></a></p>
                 </div>
-                <div className='col-sm-6'>
+                <div className='col-lg-1'></div>
+                <div className='col-lg-6 text-center'>
                     <img className="introImg"  src='./assetIntro.png'/>
                 </div>
             </div>
